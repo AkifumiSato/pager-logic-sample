10d1305b2894ccb043a96c1a15426eb39cfae4e7
佐藤昭文 <>
2019-07-30 22:47:35 +0900

    rxjs added

--- src/rx.js
+++ src/rx.js
@@ -1,23 +1,46 @@
-import debug from './debugger'
-import range from 'lodash/range'
-import flow from 'lodash/fp/flow'
-import take from 'lodash/fp/take'
-import orderBy from 'lodash/orderBy'
+import { range } from 'rxjs'
+import { filter, map } from 'rxjs/operators'
 
-const absSort = center => list => list.sort((a,b) => (Math.abs(center - a) < Math.abs(center - b) ? -1 : 1))
-
-// 引数までの数列を生成する関数を返却
-const getExpandList = (width, maxLimit, minLimit) => (center) => {
-  const max = center + width >= maxLimit ? maxLimit : center + width
-  const min = center - width < minLimit ? minLimit : center - width
-
-  return flow(
-    absSort(center),
-    take(width),
-    orderBy,
-  )(range(min, max))
+const rangeGenerator = function* (start, limit) {
+  while (start <= limit) {
+    yield start++
+  }
 }
 
-const getPagerListMax20 = getExpandList(5, 21, 0)
-const getPagerListMax3 = getExpandList(5, 3, 0)
-debug({ getPagerListMax20, getPagerListMax3 })
+const nearPage = (width, maxLimit, minLimit = 0) => value => {
+  const min = value - width < minLimit ? minLimit : value - width
+  const max = min + (width * 2 + 1) > maxLimit ? maxLimit : min + (width * 2 + 1)
+  return Array.from([...rangeGenerator(min, max)])
+    .filter((_, i) => i < 5)
+}
+
+const createPagerList = ({ max, min = 0, width }) => {
+  const getPagerList = nearPage(width, max)
+
+  console.log(getPagerList(0))
+  console.log(getPagerList(1))
+  console.log(getPagerList(19))
+  console.log(getPagerList(20))
+
+  return range(min, max)
+    .pipe(
+      map(value => ({
+        center: value + 1,
+        list: getPagerList(value + 1),
+      })),
+    )
+}
+
+const pagerList$ = createPagerList({
+  max: 20,
+  width: 2,
+})
+
+const getFilteredPageList = value => pagerList$.pipe(
+  filter(({ center }) => center === value)
+)
+
+getFilteredPageList(20).subscribe(value => console.log(value.list))
+// pagerList$.subscribe(value => console.log(value))
+
+
